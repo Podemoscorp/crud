ce1877facdccb0eab431257e371c992f94cbf50e
Julio <jn5.guape@gmail.com>
2021-02-15 20:04:28 -0300

    black .

--- core/models.py
+++ core/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils import timezone
 from user.models import User
 
-# Create your models here.
-
 
 class Noticias(models.Model):
     titulo = models.CharField(max_length=100)
