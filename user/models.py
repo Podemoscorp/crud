700c70c610fad4e47a74902818113bdd348bb918
Julio <jn5.guape@gmail.com>
2021-02-15 17:22:21 -0300

    mudanças na tabela de user

--- user/models.py
+++ user/models.py
@@ -18,24 +18,24 @@ class Role(models.Model):
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, cpf, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         """
         Create and save a user with the given username, email, and password.
         """
         if not email:
             raise ValueError("The given email must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, cpf=cpf, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, cpf=None, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, cpf, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, cpf, password, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -44,7 +44,7 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, cpf, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
     def with_perm(
         self, perm, is_active=True, include_superusers=True, backend=None, obj=None
@@ -99,19 +99,19 @@ class User(AbstractBaseUser, PermissionsMixin):
             "Unselect this instead of deleting accounts."
         ),
     )
-    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
-    soft_delet = models.DateTimeField(_("Deleted ?"), blank=True, default=None)
-    is_trusty = models.DateTimeField(_("is trusty"), default=None, blank=True)
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now, blank=True)
+    soft_delet = models.DateTimeField(_("Deleted ?"), blank=True, null=True)
+    is_trusty = models.DateTimeField(_("is trusty"), blank=True, null=True)
     cpf = models.CharField(_("CPF"), max_length=15, blank=True, unique=True)
-    endereco = models.CharField(_("Address"), max_length=100)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    endereco = models.CharField(_("Address"), max_length=100, blank=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, blank=True, null=True)
 
 
     objects = UserManager()
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["cpf","first_name","last_name"]
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = _("user")
