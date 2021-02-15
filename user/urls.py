346b991b6c4e9e87751e48d88187c1ae23aea55e
Julio <jn5.guape@gmail.com>
2021-02-15 17:22:56 -0300

    views de cadastro e login e logout 1/2

---
+++ user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from user import views
+
+urlpatterns = [
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import auth, messages
+from user.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.utils.translation import ugettext as _
+from django.utils import timezone
 
-# Create your views here.
+def cadastro( request ):
+    ...
+
+def login( request ):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    
+    if request.method == "POST":
+
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if email == "" or password == "":
+            menssagem = _("Os campos email e senha não podem ficar em branco")
+            messages.error(request, menssagem)
+            return redirect('login')
+
+        if User.objects.filter(email=email).exists():
+            email = email
+        elif User.objects.filter(cpf = email).exists():
+            email=User.objects.filter(cpf=email).values_list('email',flat=True).get()
+        else:
+            messages.error(request,"Usuario não encontrado")
+            return redirect('login')
+
+        user = auth.authenticate(request, email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            menssagem = _('login realizado com sucesso')
+            messages.success(request, menssagem)
+
+                
+            if not user.is_trusty:
+                menssagem = _('Confirme seu email para ter acesso')
+                messages.error(request,menssagem)
+
+            return redirect('main')            
+
+    return render(request, 'pages/login.html')
+
+def logout ( request ):
+    auth.logout(request)
+    return redirect('index')
+
+def confirma_email( request, token):
+    messages.success(request,'Email confirmado com sucesso')
+    return redirect('login')
