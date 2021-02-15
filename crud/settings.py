d785a5fca60c658209fbd03ca02178fc110ca0fb
Julio <jn5.guape@gmail.com>
2021-02-15 18:22:01 -0300

    configurando debuger

--- crud/settings.py
+++ crud/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get("ENV") == "development":
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     "192.168.0.105",
