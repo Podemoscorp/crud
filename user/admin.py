6b1ffd46482a372a2bd1a00077d5319a5ffd34fa
Julio <jn5.guape@gmail.com>
2021-02-15 21:25:05 -0300

    admin user

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from user.models import User, Role
 
-# Register your models here.
+
+class AdminUser(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "email",'date_joined','cpf','role')
+    list_display_links = ("id", "first_name", "last_name", "email",'date_joined','cpf','role')
+    search_fields = ("id", "first_name", "last_name", "email",'date_joined','cpf','role','endereco')
+    list_per_page = 20
+
+class AdminRole(admin.ModelAdmin):
+    list_display = ("id", "name", "value", "created")
+    list_display_links = ("id", "name", "value", "created")
+    search_fields = ("id", "name", "value", "created")
+    list_per_page = 20
+
+admin.site.register(User, AdminUser)
+admin.site.register(Role,   AdminRole)
