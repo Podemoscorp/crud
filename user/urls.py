de8d700ef3b4c7bee0c5418e75889d8ca39f2c51
Julio <jn5.guape@gmail.com>
2021-02-16 19:10:08 -0300

    novas urls

--- user/urls.py
+++ user/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("cadastro/", views.cadastro, name="cadastro"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("reset_password/", views.reset_password, name="reset_password"),
+    path("reset_password_done/", views.reset_password_done, name="reset_password_done"),
 ]
