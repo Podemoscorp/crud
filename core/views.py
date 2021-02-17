bea904655fcb930da5e49e4cfeddf9df2a75597f
Julio <jn5.guape@gmail.com>
2021-02-17 11:02:18 -0300

    criando funções de viewes

--- core/views.py
+++ core/views.py
@@ -3,38 +3,49 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 
-def index( request ):
+def index(request):
     ...
 
-def blog( request ):
+
+def blog(request):
     ...
 
-def post( request, id ):
+
+def post(request, id):
     ...
 
-def criar_post( request ):
+
+def criar_post(request):
     ...
 
-def criar_noticia( request ):
+
+def criar_noticia(request):
     ...
 
-def cursos( request ):
+
+def cursos(request):
     ...
 
-def curso( request ):
+
+def curso(request):
     ...
 
-def criar_curso( request ):
+
+def criar_curso(request):
     ...
 
-def olimpiadas( request ):
+
+def olimpiadas(request):
     ...
 
-def olimpiada( request ):
+
+def olimpiada(request):
     ...
 
-def calendario( request ):
+
+def calendario(request):
     ...
 
-def evento( request ):
+
+def evento(request):
     ...
