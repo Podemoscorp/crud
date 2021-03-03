c3d8827d3ccf54c8dfccfe1db798743fdd771bd2
Julio <jn5.guape@gmail.com>
2021-03-03 10:58:24 -0300

    black .

--- crud/settings.py
+++ crud/settings.py
@@ -230,7 +230,7 @@ credenciais = {
     "type": "service_account",
     "project_id": os.environ.get("PROJECT_ID"),
     "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
-    "private_key": str(os.environ.get("PRIVATE_KEY")).replace("\\n","\n"),
+    "private_key": str(os.environ.get("PRIVATE_KEY")).replace("\\n", "\n"),
     "client_email": os.environ.get("CLIENT_EMAIL"),
     "client_id": os.environ.get("CLIENT_ID"),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -239,9 +239,7 @@ credenciais = {
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/254642947670-compute%40developer.gserviceaccount.com",
 }
 
-GS_CREDENTIALS = service_account.Credentials.from_service_account_info(
-    credenciais
-)
+GS_CREDENTIALS = service_account.Credentials.from_service_account_info(credenciais)
 
 GS_PROJECT_ID = os.environ.get("PROJECT_ID")
 
