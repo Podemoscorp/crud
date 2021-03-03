c7299cd3a94b5e9ab48700005149747c05a78432
Julio <jn5.guape@gmail.com>
2021-03-02 22:11:29 -0300

    trocando local dos arquivos

--- core/urls.py
+++ core/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path("upload/image/", views.upload_image, name="image"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
