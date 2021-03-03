469c59ac13efddd3e08d579f17d708b644a71997
Julio <jn5.guape@gmail.com>
2021-03-02 22:11:51 -0300

    novas migrations

--- core/migrations/0006_auto_20210302_2036.py
+++ core/migrations/0006_auto_20210302_2036.py
@@ -9,44 +9,111 @@ import stdimage.models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0005_auto_20210301_2143'),
+        ("core", "0005_auto_20210301_2143"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Subject',
+            name="Subject",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nome', models.CharField(max_length=50)),
-                ('created', models.DateTimeField(blank=True, default=django.utils.timezone.now, help_text='Subject creation date', verbose_name='created in')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("nome", models.CharField(max_length=50)),
+                (
+                    "created",
+                    models.DateTimeField(
+                        blank=True,
+                        default=django.utils.timezone.now,
+                        help_text="Subject creation date",
+                        verbose_name="created in",
+                    ),
+                ),
             ],
         ),
         migrations.AlterField(
-            model_name='new',
-            name='image',
-            field=models.ImageField(blank=True, help_text='news cover image', upload_to='%Y/%m/%d/', verbose_name='Imagem'),
+            model_name="new",
+            name="image",
+            field=models.ImageField(
+                blank=True,
+                help_text="news cover image",
+                upload_to="%Y/%m/%d/",
+                verbose_name="Imagem",
+            ),
         ),
         migrations.AlterField(
-            model_name='post',
-            name='image',
-            field=stdimage.models.StdImageField(help_text='post cover image', upload_to='%Y/%m/%d/', verbose_name='Imagem'),
+            model_name="post",
+            name="image",
+            field=stdimage.models.StdImageField(
+                help_text="post cover image",
+                upload_to="%Y/%m/%d/",
+                verbose_name="Imagem",
+            ),
         ),
         migrations.CreateModel(
-            name='SubjectPost',
+            name="SubjectPost",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_in', models.DateTimeField(blank=True, default=django.utils.timezone.now)),
-                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='core.post')),
-                ('subject', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='core.subject')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "created_in",
+                    models.DateTimeField(blank=True, default=django.utils.timezone.now),
+                ),
+                (
+                    "post",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="core.post"
+                    ),
+                ),
+                (
+                    "subject",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="core.subject"
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='SubjectNew',
+            name="SubjectNew",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_in', models.DateTimeField(blank=True, default=django.utils.timezone.now)),
-                ('new', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='core.new')),
-                ('subject', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='core.subject')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "created_in",
+                    models.DateTimeField(blank=True, default=django.utils.timezone.now),
+                ),
+                (
+                    "new",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="core.new"
+                    ),
+                ),
+                (
+                    "subject",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="core.subject"
+                    ),
+                ),
             ],
         ),
     ]
