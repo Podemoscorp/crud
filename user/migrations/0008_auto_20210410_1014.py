fb9584f60bf0167eb888546f15776a3f1c2d0393
Julio <jn5.guape@gmail.com>
2021-04-12 21:15:08 -0300

    migrate esqeucida

--- user/migrations/0008_auto_20210410_1014.py
+++ user/migrations/0008_auto_20210410_1014.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0007_auto_20210408_1630'),
+        ("user", "0007_auto_20210408_1630"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='user',
-            name='points',
+            model_name="user",
+            name="points",
             field=models.IntegerField(blank=True, default=0),
         ),
     ]
