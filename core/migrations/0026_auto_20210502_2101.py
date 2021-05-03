53232e7b9f4a9f43370a284120210f40dbc10156
Julio <jn5.guape@gmail.com>
2021-05-02 21:05:45 -0300

    black .

--- core/migrations/0026_auto_20210502_2101.py
+++ core/migrations/0026_auto_20210502_2101.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0025_challenge_status'),
+        ("core", "0025_challenge_status"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='challenge',
-            name='status',
-            field=models.CharField(blank=True, choices=[('Aberto', 'A'), ('Terminado', 'B')], max_length=50),
+            model_name="challenge",
+            name="status",
+            field=models.CharField(
+                blank=True, choices=[("Aberto", "A"), ("Terminado", "B")], max_length=50
+            ),
         ),
     ]
